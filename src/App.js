4352664988d3de31881ac9aeb7b14ef5db8f62a6
Keith Vassallo <keith@icemalta.com>
2023-06-16 12:37:34 +0200

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { useMemo } from 'react';
 import './App.css';
 
+const images = [
+  { fileName: "01.jpg", link: "#", title: "Barcelona Afternoon", artist: "Joan White" },
+  { fileName: "02.jpg", link: "#", title: "Green on Yellow", artist: "John Dusseldorf" },
+  { fileName: "03.jpg", link: "#", title: "Change of Scene", artist: "Lucy Pattersfield" },
+  { fileName: "04.jpg", link: "#", title: "Into the Blue", artist: "Patrick McGee" },
+  { fileName: "05.jpg", link: "#", title: "Feeling Yellow", artist: "Joe Trumpton" },
+  { fileName: "06.jpg", link: "#", title: "The Fancy of Industry", artist: "Amerigo Fernandez" },
+  { fileName: "07.jpg", link: "#", title: "90 Degrees of Yellow", artist: "Cristoff Bergmann" },
+]
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <h1>Hello, World!</h1>
+  </>
 }
 
 export default App;
